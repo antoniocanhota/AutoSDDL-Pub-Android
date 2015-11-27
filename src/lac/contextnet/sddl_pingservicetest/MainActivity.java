19d3bd17a5d9242efade2fa1d5e54a0cafa0eab6
antoniocanhota <antoniocanhota@gmail.com>
2015-11-27 16:23:54 -0200

    Removed IP field (removed unused import)

--- src/lac/contextnet/sddl_pingservicetest/MainActivity.java
+++ src/lac/contextnet/sddl_pingservicetest/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import br.pucrio.acanhota.autosddl.commons.VehicleStatus;
